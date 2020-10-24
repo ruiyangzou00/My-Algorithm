6eaee6440805c7e9aa3044dc189b6d1ca1e7c393
kathyzou <51758668+ruiyangzou00@users.noreply.github.com>
2020-10-24 15:18:57 -0400

    Optimize from O(n2) to O(n)
    O(n2) to O(n): getting the max value by only comparing the current max and the new element (reverse)

---
+++ 1299. Replace Elements with Greatest Element on Right Side.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+# O(n) Optimized
+class Solution(object):
+    def replaceElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        rmax = -1
+        for i in range(len(arr) - 1, -1, -1):
+            res.insert(0, rmax)
+            rmax = max(arr[i], rmax)
+        return res
+
+
+# O(N2) Original
+# class Solution(object):
+#     def replaceElements(self, arr):
+#         for i in range(len(arr)-1):
+#             arr[i] = max(arr[i+1:])
+#         arr[-1] = -1
+#         return arr
+
+if __name__ == '__main__':
+    arr = [5, 6, 5, 9, 3, 1]
+    print(Solution().replaceElements(arr))
